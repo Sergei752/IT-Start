82e3dd744b1352935f296abdf4ef646944b1031b
Sergei752 <60628047+Sergei752@users.noreply.github.com>
2021-12-02 23:40:34 +0400

    Delete java/com/example/blog directory

--- java/com/example/blog/repo/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.blog.repo;
-
-import com.example.blog.models.Post;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepository extends CrudRepository<Post, Long> {
-}
